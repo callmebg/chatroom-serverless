87eca20806d60128fbc678904a5984f71eff5721
wcb_dell <497649129@qq.com>
2022-05-04 00:02:55 +0800

    记得要加双引号

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
-    NODE_ENV: 'production',
+    NODE_ENV: '"production"',
     IMG_URL: '"https://chatroom-1257987061.cos.ap-guangzhou.myqcloud.com"',
     SOCKET_URL: '"wss://service-mti0j9ns-1257987061.gz.apigw.tencentcs.com/release/websocket"',
     API_URL: '"https://service-mti0j9ns-1257987061.gz.apigw.tencentcs.com/release/api"'
